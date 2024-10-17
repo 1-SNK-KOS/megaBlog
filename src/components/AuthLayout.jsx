627633de621d591e89e7bd9726246320cd2daae6
Gautam Chaurasiya <150584596+1-SNK-KOS@users.noreply.github.com>
2024-10-17 21:34:23 +0530

    update

--- src/components/AuthLayout.jsx
+++ src/components/AuthLayout.jsx
@@ -1,18 +1,18 @@
 import { useSelector } from "react-redux"
-import { useState , useEffect } from "react"
+import { useState, useEffect } from "react"
 import { useNavigate } from "react-router-dom"
 import PropTypes from 'prop-types'
 
-function Protected({authentication,children}) {
+function Protected({ authentication = true, children }) {
     console.log("Protected :: ")
 
-    const [loader,setLoader] = useState(true);
+    const [loader, setLoader] = useState(true);
     const authStatus = useSelector(state => state.status)
     console.log(authStatus)
     const navigate = useNavigate()
 
     useEffect(() => {
-            
+
         //TODO : make it more easy to understand
         // if (authStatus ===true){
         //     navigate("/")
@@ -21,21 +21,21 @@ function Protected({authentication,children}) {
         // }
 
 
-        if(authentication && authStatus!==authentication)navigate('/login')
-            else if(!authentication && authStatus!==authentication)navigate('/')
+        if (authentication && authStatus !== authentication) navigate('/login')
+        else if (!authentication && authStatus !== authentication) navigate('/')
 
         setLoader(false)
-    },[authStatus,navigate,authentication])
+    }, [authStatus, navigate, authentication])
 
-      console.log(children)
-    return loader?<h1>Loading...</h1>: <>{children}</>
-    
+    console.log(children)
+    return loader ? <h1>Loading...</h1> : <>{children}</>
+
 
 }
 
 Protected.propTypes = {
     // children : PropTypes.element, // REVIEW : check types of Proptyauthentication : PropTypes.bool
- 
+
 }
 
 export default Protected
