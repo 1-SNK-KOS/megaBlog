f3eaf60d53dae314ab0c73ab839ef2d5a7bfb03c
Gautam Chaurasiya <150584596+1-SNK-KOS@users.noreply.github.com>
2024-10-17 19:35:49 +0530

    update

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,9 +1,9 @@
 
 
-function Container({children}) {
-  return <div className='w-full max-w-7xl mx-auto px-4'>{children}</div> ;
-   // you can write it without braket as it is only one line code 
-  
+function Container({ children }) {
+  return <div className='w-full max-w-7xl mx-auto px-4'>{children}</div>;
+  // you can write it without braket as it is only one line code 
+
 }
 
 export default Container
